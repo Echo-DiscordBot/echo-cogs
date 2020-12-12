c8b09d6b1332cd68977cdf2ecfe94feebc078952
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-12 18:48:36 +0545

    Update lyricsfinder.py

--- lyricsfinder/lyricsfinder.py
+++ lyricsfinder/lyricsfinder.py
@@ -59,8 +59,8 @@ class LyricsFinder(commands.Cog):
                                                                                      len(paged_content)))
                         paged_embeds.append(e)
                     for page in pagify(lyrics):
-                        await notify_channel.send(page)
-                await menu(notify_channel, paged_embeds, controls=DEFAULT_CONTROLS, timeout=180.0)
+                        await notify_channel.send(embed=discord.Embed(title='{} by {}'.format(title, artist), description=page, colour=await self.bot.get_embed_color(notify_channel))
+                #await menu(notify_channel, paged_embeds, controls=DEFAULT_CONTROLS, timeout=180.0)
             except discord.Forbidden:
                 return await notify_channel.send("Missing embed permissions..")
 
