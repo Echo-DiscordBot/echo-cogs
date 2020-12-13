68bda3d35b7f298a92e2a5991ec5f410afaf7310
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 18:17:50 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -23,25 +23,25 @@ class Utilities(commands.Cog):
             colour=await self.bot.get_embed_color(ctx.channel)
         )
         embed.set_author(name="IP Lookup Details")
-        embed.add_field(name="IP", value="Not available" or values['query'], inline=False)       
-        embed.add_field(name="Country", value="Not available" or values['country'], inline=True)
-        embed.add_field(name="Region", value="Not available" or values['regionName'], inline=True)
-        embed.add_field(name="City", value="Not available" or values['city'], inline=True)
-        embed.add_field(name="Latitude", value="Not available" or values['lat'], inline=True)
-        embed.add_field(name="Longitude", value="Not available" or values['lon'], inline=True)
-        embed.add_field(name="Timezone", value="Not available" or values['timezone'], inline=True)
+        embed.add_field(name="IP", value=values['query'] or "N/A", inline=False)       
+        embed.add_field(name="Country", value=values['country'] or "N/A", inline=True)
+        embed.add_field(name="Region", value=values['regionName'] or "N/A", inline=True)
+        embed.add_field(name="City", value=values['city'] or "N/A", inline=True)
+        embed.add_field(name="Latitude", value=values['lat'] or "N/A", inline=True)
+        embed.add_field(name="Longitude", value=values['lon'] or "N/A", inline=True)
+        embed.add_field(name="Timezone", value=values['timezone'] or "N/A", inline=True)
         embed.add_field(name="ISP", value=values['isp'] or "Not available", inline=True)
         await ctx.send(embed=embed)
 
-    @iplookup.error
-    async def iplookup_error(self, ctx, error):
-        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-            author = ctx.message.author.mention
-            msg = "***{0} you need too input a ip!***"
-            image = "https://66.media.tumblr.com/98c6d9e942941712e0ef9518fca97a7c/tumblr_opni85yA931v8tshbo1_400.gif"
-            embed = discord.Embed(
-                description=msg.format(author),
-                colour=await self.bot.get_embed_color(ctx.channel)
-            )
-            embed.set_image(url=image)
-            await ctx.send(embed=embed)
+#     @iplookup.error
+#     async def iplookup_error(self, ctx, error):
+#         if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+#             author = ctx.message.author.mention
+#             msg = "***{0} you need too input a ip!***"
+#             image = "https://66.media.tumblr.com/98c6d9e942941712e0ef9518fca97a7c/tumblr_opni85yA931v8tshbo1_400.gif"
+#             embed = discord.Embed(
+#                 description=msg.format(author),
+#                 colour=await self.bot.get_embed_color(ctx.channel)
+#             )
+#             embed.set_image(url=image)
+#             await ctx.send(embed=embed)
