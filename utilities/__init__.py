31c8768a7f993677af187c4512edb22b4dc70312
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 12:16:27 +0545

    Update and rename iplookup/__init__.py to utilities/__init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,4 @@
-from .lyricsfinder import LyricsFinder
+from .utilities import Utilities
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
@@ -6,4 +6,4 @@ __red_end_user_data_statement__ = (
 
 
 def setup(bot):
-    bot.add_cog(LyricsFinder(bot))
+    bot.add_cog(Utilities(bot))
