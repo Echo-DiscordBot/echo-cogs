b7a5eda253224df01335f7b660a1869243ac8c0c
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 16:50:46 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -24,7 +24,7 @@ class Utilities(commands.Cog):
         )
         embed.set_author(name="IP Lookup Details")
         embed.add_field(name="IP", value=values['query'], inline=False)       
-        embed.add_field(name="Country", value=values['country'], inline=True)
+        embed.add_field(name="Country", value=values['country'] || 'Not available', inline=True)
         embed.add_field(name="Region", value=values['regionName'], inline=True)
         embed.add_field(name="City", value=values['city'], inline=True)
         embed.add_field(name="Latitude", value=values['lat'], inline=True)
