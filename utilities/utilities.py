895b9952ff60444df03cf3508ca3f92e3fdf7a2d
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 19:28:01 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -47,132 +47,132 @@ class Utilities(commands.Cog):
     def parse_google_card(self, node):
         if node is None:
             return None
-    e = discord.Embed(colour=discord.Color.blue())
+        e = discord.Embed(colour=discord.Color.blue())
 
-        # check if it's a calculator card:
-    calculator = node.find(".//table/tr/td/span[@class='nobr']/h2[@class='r']")
-    if calculator is not None:
-        e.title = 'Calculator'
-        e.description = ''.join(calculator.itertext())
-        return e
-
-    parent = node.getparent()
-
-    # check for unit conversion card
-    unit = parent.find(".//ol//div[@class='_Tsb']")
-    if unit is not None:
-        e.title = 'Unit Conversion'
-        e.description = ''.join(''.join(n.itertext()) for n in unit)
-        return e
-
-    # check for currency conversion card
-    currency = parent.find(".//ol/table[@class='std _tLi']/tr/td/h2")
-    if currency is not None:
-        e.title = 'Currency Conversion'
-        e.description = ''.join(currency.itertext())
-        return e
-
-    # check for release date card
-    release = parent.find(".//div[@id='_vBb']")
-    if release is not None:
-        try:
-            e.description = ''.join(release[0].itertext()).strip()
-            e.title = ''.join(release[1].itertext()).strip()
+            # check if it's a calculator card:
+        calculator = node.find(".//table/tr/td/span[@class='nobr']/h2[@class='r']")
+        if calculator is not None:
+            e.title = 'Calculator'
+            e.description = ''.join(calculator.itertext())
             return e
-        except:
-            return None
 
-    # check for definition card
-    words = parent.find(".//ol/div[@class='g']/div/h3[@class='r']/div")
-    if words is not None:
-        try:
-            definition_info = words.getparent().getparent()[1]
-        except:
-            pass
-        else:
+        parent = node.getparent()
+
+        # check for unit conversion card
+        unit = parent.find(".//ol//div[@class='_Tsb']")
+        if unit is not None:
+            e.title = 'Unit Conversion'
+            e.description = ''.join(''.join(n.itertext()) for n in unit)
+            return e
+
+        # check for currency conversion card
+        currency = parent.find(".//ol/table[@class='std _tLi']/tr/td/h2")
+        if currency is not None:
+            e.title = 'Currency Conversion'
+            e.description = ''.join(currency.itertext())
+            return e
+
+        # check for release date card
+        release = parent.find(".//div[@id='_vBb']")
+        if release is not None:
             try:
-                e.title = words[0].text
-                e.description = words[1].text
+                e.description = ''.join(release[0].itertext()).strip()
+                e.title = ''.join(release[1].itertext()).strip()
+                return e
             except:
                 return None
-            for row in definition_info:
-                if len(row.attrib) != 0:
-                    break
+
+        # check for definition card
+        words = parent.find(".//ol/div[@class='g']/div/h3[@class='r']/div")
+        if words is not None:
+            try:
+                definition_info = words.getparent().getparent()[1]
+            except:
+                pass
+            else:
                 try:
-                    data = row[0]
-                    lexical_category = data[0].text
-                    body = []
-                    for index, definition in enumerate(data[1], 1):
-                        body.append('%s. %s' % (index, definition.text))
-                    e.add_field(name=lexical_category, value='\n'.join(body), inline=False)
+                    e.title = words[0].text
+                    e.description = words[1].text
                 except:
-                    continue
+                    return None
+                for row in definition_info:
+                    if len(row.attrib) != 0:
+                        break
+                    try:
+                        data = row[0]
+                        lexical_category = data[0].text
+                        body = []
+                        for index, definition in enumerate(data[1], 1):
+                            body.append('%s. %s' % (index, definition.text))
+                        e.add_field(name=lexical_category, value='\n'.join(body), inline=False)
+                    except:
+                        continue
+                return e
+
+        # check for translate card
+        words = parent.find(".//ol/div[@class='g']/div/table/tr/td/h3[@class='r']")
+        if words is not None:
+            e.title = 'Google Translate'
+            e.add_field(name='Input', value=words[0].text,  inline=True)
+            e.add_field(name='Out', value=words[1].text,  inline=True)
             return e
 
-    # check for translate card
-    words = parent.find(".//ol/div[@class='g']/div/table/tr/td/h3[@class='r']")
-    if words is not None:
-        e.title = 'Google Translate'
-        e.add_field(name='Input', value=words[0].text,  inline=True)
-        e.add_field(name='Out', value=words[1].text,  inline=True)
-        return e
+        # check for "time in" card
+        time_in = parent.find(".//ol//div[@class='_Tsb _HOb _Qeb']")
+        if time_in is not None:
+            try:
+                time_place = ''.join(time_in.find("span[@class='_HOb _Qeb']").itertext()).strip()
+                the_time = ''.join(time_in.find("div[@class='_rkc _Peb']").itertext()).strip()
+                the_date = ''.join(time_in.find("div[@class='_HOb _Qeb']").itertext()).strip()
+            except:
+                return None
+            else:
+                e.title = time_place
+                e.description = '%s\n%s' % (the_time, the_date)
+                return e
 
-    # check for "time in" card
-    time_in = parent.find(".//ol//div[@class='_Tsb _HOb _Qeb']")
-    if time_in is not None:
+        weather = parent.find(".//ol//div[@class='e']")
+        if weather is None:
+            return None
+
+        location = weather.find('h3')
+        if location is None:
+            return None
+
+        e.title = ''.join(location.itertext())
+
+        table = weather.find('table')
+        if table is None:
+            return None
+
         try:
-            time_place = ''.join(time_in.find("span[@class='_HOb _Qeb']").itertext()).strip()
-            the_time = ''.join(time_in.find("div[@class='_rkc _Peb']").itertext()).strip()
-            the_date = ''.join(time_in.find("div[@class='_HOb _Qeb']").itertext()).strip()
+            tr = table[0]
+            img = tr[0].find('img')
+            category = img.get('alt')
+            image = 'https:' + img.get('src')
+            temperature = tr[1].xpath("./span[@class='wob_t']//text()")[0]
         except:
             return None
         else:
-            e.title = time_place
-            e.description = '%s\n%s' % (the_time, the_date)
-            return e
+            e.set_thumbnail(url=image)
+            e.description = '*%s*' % category
+            e.add_field(name='Temperature', value=temperature)
 
-    weather = parent.find(".//ol//div[@class='e']")
-    if weather is None:
-        return None
+        try:
+            wind = ''.join(table[3].itertext()).replace('Wind: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Wind', value=wind)
 
-    location = weather.find('h3')
-    if location is None:
-        return None
+        try:
+            humidity = ''.join(table[4][0].itertext()).replace('Humidity: ', '')
+        except:
+            return None
+        else:
+            e.add_field(name='Humidity', value=humidity)
 
-    e.title = ''.join(location.itertext())
-
-    table = weather.find('table')
-    if table is None:
-        return None
-
-    try:
-        tr = table[0]
-        img = tr[0].find('img')
-        category = img.get('alt')
-        image = 'https:' + img.get('src')
-        temperature = tr[1].xpath("./span[@class='wob_t']//text()")[0]
-    except:
-        return None
-    else:
-        e.set_thumbnail(url=image)
-        e.description = '*%s*' % category
-        e.add_field(name='Temperature', value=temperature)
-
-    try:
-        wind = ''.join(table[3].itertext()).replace('Wind: ', '')
-    except:
-        return None
-    else:
-        e.add_field(name='Wind', value=wind)
-
-    try:
-        humidity = ''.join(table[4][0].itertext()).replace('Humidity: ', '')
-    except:
-        return None
-    else:
-        e.add_field(name='Humidity', value=humidity)
-
-    return e
+        return e
 
     async def get_google_entries(self, query):
         params = {
