aa9a363044382f374966069f3791e24b5c40040f
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 12:23:39 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -22,12 +22,12 @@ class Utilities(commands.Cog):
       if ip is None:
           print('\n[LOGS] Must enter a ip!')
           await ctx.send('Must enter a ip!')
-        else:
-            print(f'\n[LOGS] Running whois on {ip}')
-            host = socket.gethostbyname(ip)
-            w = IPWhois(host)
-            res = w.lookup_whois(inc_nir=True)
-            final_res = """
+      else:
+          print(f'\n[LOGS] Running whois on {ip}')
+          host = socket.gethostbyname(ip)
+          w = IPWhois(host)
+          res = w.lookup_whois(inc_nir=True)
+          final_res = """
     IP: {}
     IP Range: {}
     Name: {}
@@ -41,8 +41,8 @@ class Utilities(commands.Cog):
     City: {}
     Address: {}
     Postal Code: {}
-            """.format(res['query'], res['nets'][0]['range'], res['nets'][0]['name'], res['nets'][0]['handle'], res['asn_registry'], res['asn_description'], res['asn_date'], res['nets'][0]['updated'], res['nets'][0]['country'], res['nets'][0]['state'], res['nets'][0]['city'], res['nets'][0]['address'], res['nets'][0]['postal_code'])
-            print(final_res)
-            await ctx.send(final_res)
+          """.format(res['query'], res['nets'][0]['range'], res['nets'][0]['name'], res['nets'][0]['handle'], res['asn_registry'], res['asn_description'], res['asn_date'], res['nets'][0]['updated'], res['nets'][0]['country'], res['nets'][0]['state'], res['nets'][0]['city'], res['nets'][0]['address'], res['nets'][0]['postal_code'])
+          print(final_res)
+          await ctx.send(final_res)
         
  
