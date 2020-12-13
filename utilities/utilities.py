bdf611e1658842fb045a4c86887fb2eb3861affb
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-13 18:40:56 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -12,7 +12,7 @@ class Utilities(commands.Cog):
         #with open("utilities/gifs/error.txt") as f:
         #    self.choices_error = f.readlines()
 
-    @commands.command()
+    @commands.command(name="iplookup", aliases=["ip", "ipinfo")
     async def iplookup(self, ctx, arg):
         if arg=="mine":
             arg = ""
@@ -24,15 +24,22 @@ class Utilities(commands.Cog):
         embed = discord.Embed(
             colour=await self.bot.get_embed_color(ctx.channel)
         )
-        embed.set_author(name="IP Lookup Details")
-        embed.add_field(name="IP", value=values['query'] or "N/A", inline=False)       
+        embed.set_author(name="🌐 IP Lookup Details")
+        embed.add_field(name="IP", value=values['query'] or "N/A", inline=False)
+        embed.add_field(name="Mobile", value=values['mobile'] or "N/A", inline=True)
+        embed.add_field(name="Proxy", value=values['proxy'] or "N/A", inline=True)
+        embed.add_field(name="Hosting", value=values['hosting'] or "N/A", inline=True)
+        embed.add_field(name="Continent", value=values['continent'] or "N/A", inline=True)
         embed.add_field(name="Country", value=values['country'] or "N/A", inline=True)
         embed.add_field(name="Region", value=values['regionName'] or "N/A", inline=True)
         embed.add_field(name="City", value=values['city'] or "N/A", inline=True)
+        embed.add_field(name="District", value=values['district'] or "N/A", inline=True)
+        embed.add_field(name="Zip", value=values['zip'] or "N/A", inline=True)
         embed.add_field(name="Latitude", value=values['lat'] or "N/A", inline=True)
         embed.add_field(name="Longitude", value=values['lon'] or "N/A", inline=True)
-        embed.add_field(name="Timezone", value=values['timezone'] or "N/A", inline=True)
-        embed.add_field(name="ISP", value=values['isp'] or "Not available", inline=True)
+        embed.add_field(name="💸 Currency", value=values['currency'] or "N/A", inline=True)
+        embed.add_field(name="⏲️ Timezone", value=values['timezone'] or "N/A", inline=True)
+        embed.add_field(name="🌐 ISP", value=values['isp'] or "Not available", inline=True)
         await ctx.send(embed=embed)
 
 #     @iplookup.error
