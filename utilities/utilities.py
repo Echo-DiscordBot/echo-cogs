4398a5f3bd2c8af4660d21a080d0b419ae842a1a
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-18 16:52:06 +0545

    Update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -5,26 +5,14 @@ import json
 import requests
 import aiohttp
 import time
-from redbot.core import commands, Config
+from redbot.core import checks, commands, Config
 from datetime import datetime
+from TagScriptEngine import Interpreter, adapter, block
 
 
 class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        #with open("utilities/gifs/error.txt") as f:
-        #    self.choices_error = f.readlines()
-    
-    def acquire_session(self):
-        # will error if you run outside event loop:
-        asyncio.get_running_loop()
-        if not hasattr(self, "_session"):
-            self._session = aiohttp.ClientSession(
-                connector = aiohttp.TCPConnector(
-                    limit = self._pool_size,
-                )
-            )
-        return self._session
 
     @commands.command(name="iplookup", aliases=["ip", "ipinfo"])
     async def iplookup(self, ctx, arg):
@@ -55,75 +43,20 @@ class Utilities(commands.Cog):
         embed.set_footer(text=f"Requested by: {ctx.message.author}", icon_url=ctx.message.author.avatar_url)
         await ctx.send(embed=embed)
         
-    @commands.command(aliases=["ipaddr"])
-    @commands.cooldown(rate=3, per=5, type=commands.BucketType.user)
-    async def ipnew(self, ctx, ip: str = None):
-        """
-        Find out the information of an IP Address
-        API Provided by: https://ipapi.co/
-        """
-        if ip is None:
-            await ctx.send(embed=discord.Embed(description="⚠ Please Specify the IP Address!"))
-            return
+    @commands.command(aliases=["calc"])
+    async def calculate(self, ctx, *, query):
+        """Math"""
+        query = query.replace(",", "")
+        engine_input = "{m:" + query + "}"
+        start = time.monotonic()
+        output = self.engine.process(engine_input)
+        end = time.monotonic()
 
-        if ip == "0.0.0.0" or ip == "127.0.0.1":
-            await ctx.send(embed=discord.Embed(description="You have played yourself. Wait... You can't!"))
-            return
-
-        await ctx.trigger_typing()
-
-        try:
-            lookup = ("http://ipapi.co/" + ip + "/json/")
-            data = requests.get(lookup).json()
-#             session = self.acquire_session()
-#             session.get(f'https://ipapi.co/{ip}/json/') as resp:
-#                 resp.raise_for_status()
-#                 data = json.loads(await resp.read(), object_hook=DictObject)
-                    
-            ipaddr = data["ip"]
-            city = data["city"]
-            region = data["region"]
-            region_code = data["region_code"]
-            country = data["country"]
-            country_name = data["country_name"]
-            country_code_iso3 = data["country_code_iso3"]
-            continent_code = data["continent_code"]
-            in_eu = data["in_eu"]
-            postal = data["postal"]
-            latitude = data["latitude"]
-            longitude = data["longitude"]
-            country_timezone = data["timezone"]
-            utc_offset = data["utc_offset"]
-            dial_code = data["country_calling_code"]
-            currency = data["currency"]
-            languages = data["languages"]
-            organization = data["org"]
-            asn = data["asn"]
-
-            embd = discord.Embed(title="IP Information", color=ctx.author.color, timestamp=datetime.utcnow())
-            embd.add_field(name="IP Address:", value=ipaddr, inline=True)
-            embd.add_field(name="ISP Name/Organization:", value=organization, inline=True)
-            embd.add_field(name="City:", value=city, inline=True)
-            embd.add_field(name="Regional Area:", value=region, inline=True)
-            embd.add_field(name="Region Code:", value=region_code, inline=True)
-            embd.add_field(name="Country:", value=country, inline=True)
-            embd.add_field(name="Country Name:", value=country_name, inline=True)
-            embd.add_field(name="Country Code (ISO):", value=country_code_iso3, inline=True)
-            embd.add_field(name="Language Spoken:", value=languages, inline=True)
-            embd.add_field(name="Continent Code:", value=continent_code, inline=True)
-            embd.add_field(name="Is country a member of European Union (EU)?", value=in_eu, inline=True)
-            embd.add_field(name="Postal Code:", value=postal, inline=True)
-            embd.add_field(name="Latitude Coordinate:", value=latitude, inline=True)
-            embd.add_field(name="Longitude Coordinate:", value=longitude, inline=True)
-            embd.add_field(name="Timezone:", value=country_timezone, inline=True)
-            embd.add_field(name="UTC Offset:", value=utc_offset, inline=True)
-            embd.add_field(name="Country Dial Code:", value=dial_code, inline=True)
-            embd.add_field(name="Currency:", value=currency, inline=True)
-            embd.add_field(name="Autonomous System Number:", value=asn, inline=True)
-            embd.set_footer(text=f"Requested by: {ctx.message.author}", icon_url=ctx.message.author.avatar_url)
-
-            await ctx.send(embed=embd)
-        except IndexError:
-            await ctx.send(embed=discord.Embed(description="⚠ An Error Occured! Make sure the IP and the formatting are correct!"))
-        except KeyError:
-            await ctx.send(embed=discord.Embed(description="⚠ An Error Occured! Make sure the IP and the formatting are correct!"))
+        output_string = output.body.replace("{m:", "").replace("}", "")
+        e = discord.Embed(
+            color=await ctx.embed_color(),
+            title=f"Input: `{query}`",
+            description=f"Output: `{output_string}`",
+        )
+        e.set_footer(text=f"Calculated in {round((end - start) * 1000, 3)} ms")
+        await ctx.send(embed=e)
