1d6b19ea6343b5091afba5dd7a5ab48ae8eddb38
Anamol Sapkota <17160107+anamolsapkota@users.noreply.github.com>
2020-12-21 19:02:59 +0545

    Create __init__.py

---
+++ google/__init__.py
@@ -0,0 +1,19 @@
+{
+  "author": ["anamolsapkota"],
+  "install_msg": "Thank you for installing google. Type `[p]help Google` for more information.",
+  "name": "Google",
+  "short": "Search anything on google",
+  "description": "Search anything on google",
+  "disabled": false,
+  "tags": [
+    "search", "google"
+  ],
+  "requirements": [
+    "bs4",
+    "requests-futures"
+  ],
+  "min_python_version": [
+    3, 6, 0
+  ],
+  "end_user_data_statement": "This cog does not persistently store data or metadata about users."
+}
